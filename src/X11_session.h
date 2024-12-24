02b404ed845b8c81bf2437b720ae7d64553aa912
ardinugraha <0x4rd1@gmail.com>
2024-12-24 22:00:20 +0700

    Add GPLv3 license

--- src/X11_session.h
+++ src/X11_session.h
@@ -1,4 +1,23 @@
 
+/*
+ * This file is part of winmgm.
+ *
+ * winmgm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * winmgm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with winmgm.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Ardinugraha
+ */
+
 #include "winmgm.h"
 #include <X11/X.h>
 #include <X11/Xatom.h>
